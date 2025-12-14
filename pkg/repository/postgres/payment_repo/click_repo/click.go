41c6c626d8507875d82e15a0827df62d26267a84
bahodir <isomiddinovbahodir7@gmail.com>
2025-12-14 14:51:29 +0500

    fix click

--- pkg/repository/postgres/payment_repo/click_repo/click.go
+++ pkg/repository/postgres/payment_repo/click_repo/click.go
@@ -66,7 +66,7 @@ func (r repo) Create(ctx context.Context, req structs.Invoice) (int64, error) {
 		) VALUES (
 			$1, $2, $3, $4, $5,
 			$6, $7, $8, $9, $10,
-			$11, $12, $13, $14
+			$11, $12, $13
 		) RETURNING id
 	`
 	var id int64
